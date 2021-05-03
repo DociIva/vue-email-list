920a26ed2db62c5ca41c548e23c04ea6ed177e71
DociIva <doci.iva@hotmail.com>
2021-05-04 00:13:50 +0200

    add-modif2

--- js/script.js
+++ js/script.js
@@ -9,42 +9,45 @@ const app = new Vue({
     el: '#app',
     data: { 
        email: [],
-    }, // per chiamare Api
+    }, 
+     // per chiamare Api
     created() {
      
-    
-    for(let i = 0; i <= 10; i++) {
-        // chiamata | GET con che metodo
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        for(let i = 0; i < 10; i++) {
 
-        //Then funzioni di callback per i casi di successo | catch istruzione ti consente di gestire l'errore
-        .then(result => {
-            console.log(result.data);
-           
-            // cosa sto prendendo fai la referenza dentro
-           this.referenza = result.data.response;
-           console.log(this.referenza); 
+            // chiamata | GET con che metodo
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+ 
+             //Then funzioni di callback per i casi di successo | catch istruzione ti consente di gestire l'errore
+            .then(result => {
+               //console.log(result.data);
             
-
-            // mettere dentro l'array tutti gli elementi
-            // prendi l'array vuoto e e metti denttro la referenza 
+              // cosa sto prendendo fai la referenza dentro
+                this.referenza = result.data.response;
+                //console.log(this.referenza); 
+             
+ 
+               // mettere dentro l'array tutti gli elementi
+                // prendi l'array vuoto e e metti denttro la referenza 
+             
+               this.email.push(this.referenza);
+               //console.log( this.email.push(this.referenza));
+             
+            })
             
-            this.email.push(this.referenza);
-            console.log( this.email.push(this.result));
-            
-        })
-        .catch(error => {
-            //errore
-            console.log(error);
-        })
         
-        
-    }
-
+        };
+            
     },
     methods: {
 
     }
+
+        
+    
+    
+
+
    
 });
 
