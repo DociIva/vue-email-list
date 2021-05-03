0207703db60e8fb3ec3877b3fa2adbd79b1bac78
DociIva <doci.iva@hotmail.com>
2021-05-03 23:45:43 +0200

    add-mod

--- js/script.js
+++ js/script.js
@@ -4,4 +4,49 @@
 // https://flynn.boolean.careers/exercises/api/random/mail 
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista con Vue JS e Axios.
 
-// A 
+// A istanza Vue
+const app = new Vue({
+    el: '#app',
+    data: { 
+       email: [],
+    }, // per chiamare Api
+    created() {
+     
+    
+    for(let i = 0; i <= 10; i++) {
+        // chiamata | GET con che metodo
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+        //Then funzioni di callback per i casi di successo | catch istruzione ti consente di gestire l'errore
+        .then(result => {
+            console.log(result.data);
+           
+            // cosa sto prendendo fai la referenza dentro
+           this.referenza = result.data.response;
+           console.log(this.referenza); 
+            
+
+            // mettere dentro l'array tutti gli elementi
+            // prendi l'array vuoto e e metti denttro la referenza 
+            
+            this.email.push(this.referenza);
+            console.log( this.email.push(this.result));
+            
+        })
+        .catch(error => {
+            //errore
+            console.log(error);
+        })
+        
+        
+    }
+
+    },
+    methods: {
+
+    }
+   
+});
+
+
+ 
