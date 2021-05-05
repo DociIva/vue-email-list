8c3225630cdfdd9fb7a8a6d63e36bf83bac949d4
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-05 08:45:17 +0200

    add-corection

--- js/script.js
+++ js/script.js
@@ -9,31 +9,39 @@ const app = new Vue({
     el: '#app',
     data: { 
        email: [],
+       emails: 10,
+       
     }, 
      // per chiamare Api
-    created() {
-     
-        for(let i = 0; i < 10; i++) {
+    created() { 
+        
+    
+         
+        for(let i = 0; i < 10 ; i++) {
 
             // chiamata | GET con che metodo
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
  
              //Then funzioni di callback per i casi di successo | catch istruzione ti consente di gestire l'errore
-            .then(result => {
+              .then(result => {
+
                //console.log(result.data);
-            
-              // cosa sto prendendo fai la referenza dentro
-                this.referenza = result.data.response;
-                //console.log(this.referenza); 
+               
+                 //this.email.push(result.data.response);
+                 //console.log(this.email)
+                 //cosa sto prendendo fai la referenza dentro
+                 this.referenza = result.data.response;
+                 console.log(this.referenza); 
              
- 
-               // mettere dentro l'array tutti gli elementi
-                // prendi l'array vuoto e e metti denttro la referenza 
-             
-               this.email.push(this.referenza);
-               //console.log( this.email.push(this.referenza));
+                  
+                 // mettere dentro l'array tutti gli elementi
+                 // prendi l'array vuoto e e metti denttro la referenza 
+                   this.email.push(this.referenza);
+                 
+                 // console.log(this.referenza);
              
             })
+            
             
         
         };
